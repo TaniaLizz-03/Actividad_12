9b373397e9a0b15eaedbef0d22caf432632b1978
TaniaLizz-03 <71456511+TaniaLizz-03@users.noreply.github.com>
2020-10-13 16:29:56 -0600

    Add files via upload

---
+++ Actividad 12/script.js
@@ -0,0 +1,26 @@
+const bicicleta = () =>{
+    setTimeout(()=>{
+    console.log("Comprar Bicicleta");
+    }, 5000);
+}
+
+const casco = () =>{
+    setTimeout(()=>{
+    console.log("Comprar Casco");
+    }, 1000);
+}
+
+const guantes = () =>{
+    setTimeout(()=>{
+    console.log("Comprar Guantes");
+    }, 2000);
+}
+
+const pasear = () =>{
+    setTimeout(()=>{
+    console.log("Ir de paseo en Bicicleta");
+    }, 8000);
+}
+
+
+
